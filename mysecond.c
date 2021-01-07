e3400f4c5c5d27015503d6c977495fd6a54ac88e
Michael Klemm <michael@dontknow.de>
2021-01-07 10:17:43 +0100

    Proper formatting the source code

--- mysecond.c
+++ mysecond.c
@@ -1,27 +1,32 @@
 /* A gettimeofday routine to give access to the wall
    clock timer on most UNIX-like systems.
 
-   This version defines two entry points -- with 
+   This version defines two entry points -- with
    and without appended underscores, so it *should*
    automagically link with FORTRAN */
 
 #include <sys/time.h>
 
-double mysecond()
-{
-/* struct timeval { long        tv_sec;
-            long        tv_usec;        };
+double mysecond(void ) {
+    /* struct timeval {
+          long        tv_sec;
+          long        tv_usec;
+        };
 
-struct timezone { int   tz_minuteswest;
-             int        tz_dsttime;      };     */
+      struct timezone {
+          int   tz_minuteswest;
+          int        tz_dsttime;
+      };
+    */
 
-        struct timeval tp;
-        struct timezone tzp;
-        int i;
+    struct timeval tp;
+    struct timezone tzp;
+    int i;
 
-        i = gettimeofday(&tp,&tzp);
-        return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
+    i = gettimeofday(&tp,&tzp);
+    return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
 }
 
-double mysecond_() {return mysecond();}
-
+double mysecond_(void) {
+    return mysecond();
+}
